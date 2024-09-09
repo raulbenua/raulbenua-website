ffdc56a905d80114b5c7acdd80e8beacac16762e
Jerson La Torre <jerson.latorre@gmail.com>
2024-09-09 01:33:31 -0500

    add: se agregaron todas las imágenes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,35 @@
 'use client'
 
-import Footer from './components/Footer'
-import Gallery from './components/Gallery'
-import Header from './components/Header'
-import ProgressBar from './components/ProgressBar'
+import { useEffect, useState } from 'react'
+
+import Footer from '@/app/components/Footer'
+import Gallery from '@/app/components/Gallery'
+import Header from '@/app/components/Header'
+import ProgressBar from '@/app/components/ProgressBar'
 import { motion } from 'framer-motion'
-import useImagesLoader from './hooks/useImagesLoader'
-
-const imagesH = [
-  '/images/h/RaulBenua_geographiesofwater_ph1.jpg',
-  '/images/h/RaulBenua_geographiesofwater_ph2.jpg',
-  '/images/h/RaulBenua_geographiesofwater_ph3.jpg',
-  '/images/h/RaulBenua_geographiesofwater_ph4.jpg',
-  '/images/h/RaulBenua_geographiesofwater_ph5.jpg',
-  '/images/h/RaulBenua_geographiesofwater_ph6.jpg',
-  '/images/h/RaulBenua_geographiesofwater_ph7.jpg',
-]
-
-const imagesV = [
-  '/images/v/RaulBenua_geographiesofwater_pv1.jpg',
-  '/images/v/RaulBenua_geographiesofwater_pv2.jpg',
-  '/images/v/RaulBenua_geographiesofwater_pv3.jpg',
-  '/images/v/RaulBenua_geographiesofwater_pv4.jpg',
-  '/images/v/RaulBenua_geographiesofwater_pv5.jpg',
-  '/images/v/RaulBenua_geographiesofwater_pv6.jpg',
-  '/images/v/RaulBenua_geographiesofwater_pv7.jpg',
-]
+import useImagesLoader from '@/app/hooks/useImagesLoader'
 
 export default function Home() {
-  const { loadedImages, loadingProgress } = useImagesLoader([...imagesH, ...imagesV])
+  const [content, setContent] = useState<{ imagesH: string[]; imagesV: string[] } | null>(null)
+  const { loadedImages, loadingProgress } = useImagesLoader(content ? [...content.imagesH, ...content.imagesV] : [])
 
-  if (!loadedImages) {
+  useEffect(() => {
+    const fetchContent = async () => {
+      const response = await fetch('/content.json')
+      const data = await response.json()
+      setContent(data)
+    }
+    fetchContent()
+  }, [])
+
+  if (!content || !loadedImages) {
     return <ProgressBar loadingProgress={loadingProgress} />
   }
 
   return (
     <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.6, ease: 'easeInOut' }}>
       <Header />
-      <Gallery imagesH={imagesH} imagesV={imagesV} />
+      <Gallery imagesH={content.imagesH} imagesV={content.imagesV} />
       <Footer />
     </motion.div>
   )
