db24353436c232036024a5a400998125272d7ed8
Jerson La Torre <jerson.latorre@gmail.com>
2024-09-09 22:42:15 -0500

    test: forzar a que las imagenes se recarguen

--- src/app/hooks/useImagesLoader.ts
+++ src/app/hooks/useImagesLoader.ts
@@ -17,7 +17,7 @@ export default function useImagesLoader(images: string[]) {
     const loadImage = (src: string) => {
       return new Promise<void>((resolve, reject) => {
         const img = new Image()
-        img.src = src
+        img.src = src + '?t=' + Math.random()
         img.onload = () => resolve()
         img.onerror = () => reject(new Error(`Failed to load image: ${src}`))
       })
