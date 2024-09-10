f9c772b5c36cbf8f4f8294639c802c692b291617
Jerson La Torre <jerson.latorre@gmail.com>
2024-09-09 22:53:27 -0500

    nueva pruenba de cargdador

--- src/app/hooks/useImagesLoader.ts
+++ src/app/hooks/useImagesLoader.ts
@@ -1,14 +1,12 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function useImagesLoader(images: string[]) {
   const [loadedImages, setLoadedImages] = useState(false)
   const [loadingProgress, setLoadingProgress] = useState(0)
   const [error, setError] = useState<string | null>(null)
-  const isMounted = useRef(true)
 
   useEffect(() => {
-    isMounted.current = true
-
+    // Maneja el caso en el que no hay imágenes para cargar
     if (images.length === 0) {
       setLoadedImages(true)
       setLoadingProgress(100)
@@ -19,7 +17,9 @@ export default function useImagesLoader(images: string[]) {
       return new Promise<void>((resolve, reject) => {
         const img = new Image()
         img.src = src + '?t=' + Math.random() // Evita el caché
-        img.onload = () => resolve()
+        img.onload = () => {
+          resolve()
+        }
         img.onerror = () => reject(new Error(`Failed to load image: ${src}`))
       })
     }
@@ -28,31 +28,19 @@ export default function useImagesLoader(images: string[]) {
       let loadedCount = 0
 
       for (const src of images) {
-        if (!isMounted.current) return
         try {
           await loadImage(src)
           loadedCount++
           setLoadingProgress(Math.round((loadedCount / images.length) * 100))
         } catch (error) {
-          if (isMounted.current) {
-            setError((error as Error).message)
-            for (let i = loadedCount; i < images.length; i++) {
-              await loadImage(images[i])
-            }
-          }
+          setError((error as Error).message)
         }
       }
 
-      if (isMounted.current) {
-        setLoadedImages(true)
-      }
+      setLoadedImages(true)
     }
 
     loadAllImages()
-
-    return () => {
-      isMounted.current = false
-    }
   }, [images])
 
   return { loadedImages, loadingProgress, error }
